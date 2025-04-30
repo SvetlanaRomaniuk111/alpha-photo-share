45733675b3d87df0739b921f8ecce6abd29f70ef
ink404dot <94466285+ink404dot@users.noreply.github.com>
2025-04-30 09:13:54 +0300

    Add database and Redis session management classes

---
+++ src/db/database.py
@@ -0,0 +1,39 @@
+import contextlib
+
+from sqlalchemy.ext.asyncio import (
+    AsyncEngine,
+    async_sessionmaker,
+    create_async_engine,
+)
+
+from src.core import log, config
+
+
+class DatabaseSessionManager:
+    def __init__(self, url: str):
+        self._engine: AsyncEngine | None = create_async_engine(url, echo=True)
+        self._session_maker: async_sessionmaker = async_sessionmaker(
+            autoflush=False, autocommit=False, bind=self._engine
+        )
+
+    @contextlib.asynccontextmanager
+    async def session(self):
+        if self._session_maker is None:
+            raise Exception("Session is not initialized")
+        session = self._session_maker()
+        try:
+            yield session
+        except Exception as err:
+            log.error(err)
+            await session.rollback()
+            raise
+        finally:
+            await session.close()
+
+
+sessionmanager = DatabaseSessionManager(config.db_config.DATABASE_URL)
+
+
+async def get_db():
+    async with sessionmanager.session() as session:
+        yield session
---
+++ src/db/redis.py
@@ -0,0 +1,46 @@
+from redis.asyncio import Redis
+import contextlib
+from src.core import config, log
+
+
+class RedisSessionManager:
+    def __init__(self, host: str, port: int, db: int, password: str | None = None):
+        self._redis_client = Redis(
+            host=host,
+            port=port,
+            db=db,
+            password=password,
+        )
+
+    async def connect(self):
+        """Опциональный метод, если нужно явно проверять подключение"""
+        try:
+            await self._redis_client.ping()
+        except Exception as e:
+            log.error(f"Ошибка подключения к Redis: {e}")
+            
+    async def close(self):
+        if self._redis_client:
+            await self._redis_client.close()
+
+    @contextlib.asynccontextmanager
+    async def session(self):
+        if self._redis_client is None:
+            raise Exception("Redis is not initialized")
+        yield self._redis_client
+
+
+
+# Створюємо глобальний об'єкт менеджера Redis
+redis_manager = RedisSessionManager(
+    host=config.redis_config.REDIS_HOST,
+    port=config.redis_config.REDIS_PORT,
+    db=config.redis_config.REDIS_DB,
+    password=config.redis_config.REDIS_PASSWORD
+)
+
+
+async def get_redis():
+    """Функція-залежність для отримання сесії Redis у FastAPI"""
+    async with redis_manager.session() as redis:
+        yield redis
