154eed20a91b5f6d7d3f4b454079b8cb55a64162
Ilya Dudelzak <65915836+IlyaDudelzak@users.noreply.github.com>
2025-05-02 14:39:42 +0300

    Update qr_code.py
    Removed function GetOrCreateQrcode

--- src/services/qr_code.py
+++ src/services/qr_code.py
@@ -44,15 +44,5 @@ class QrCodeService:
         img = qr.make_image(fill_color="black", back_color="white")
         svg_string = img.to_string().decode('utf-8').replace('width="37mm"', 'width="200px"').replace('height="37mm"', 'height="200px"').replace('fill_color="black"', 'fill="#000000"').replace('back_color="white"', '')
         return svg_string
-    
-    def GetOrCreateQrCode(self, data: str) -> str:
-        """
-        Получает или создает QR-код на основе переданных данных.
-
-        :param data: Данные для кодирования в QR-код.
-        :return: Путь к сгенерированному QR-коду.
-        """
-        qrcode_img = self.GenerateQrCode(data)
-        return qrcode_img
 
 qrcode_service = QrCodeService()
