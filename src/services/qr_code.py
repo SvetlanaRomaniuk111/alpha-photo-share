bf28cad5cc30b4e0ef6a75ef8be50db0a75187a5
Ilya Dudelzak <DIA070611@gmail.com>
2025-05-02 19:19:17 +0000

    Refactor QR code generation methods for improved clarity and maintainability

--- src/services/qr_code.py
+++ src/services/qr_code.py
@@ -1,6 +1,7 @@
 import qrcode
 from qrcode.image.svg import SvgPathFillImage
 from qrcode.image.pil import PilImage
+import asyncio
 
 class QrCodeService:
     def __init__(self):
@@ -9,41 +10,6 @@ class QrCodeService:
         """
         pass
 
-    def generatePilImage(self, data:str) -> PilImage:
-        """
-        Генерирует QR-код на основе переданных данных.
-
-        :param data: Данные для кодирования в QR-код.
-        :return: Путь к сгенерированному QR-коду.
-        """
-        qr = qrcode.QRCode(
-            error_correction=qrcode.constants.ERROR_CORRECT_H,
-            box_size=10,
-            border=4,
-        )
-        qr.add_data(data)
-        qr.make(fit=True)
-        return qr.make_image(fill_color="black", back_color="white")
-    
-    def generateSvg(self, data:str, size:int=200) -> str:
-        """
-        Генерирует QR-код в формате SVG на основе переданных данных (фиксированные размеры и стандартные атрибуты).
-
-        :param data: Данные для кодирования в QR-код.
-        :return: Строка с SVG-кодом QR-кода.
-        """
-        qr = qrcode.QRCode(
-            image_factory=SvgPathFillImage,
-            error_correction=qrcode.constants.ERROR_CORRECT_H,
-            box_size=10,
-            border=4,
-        )
-        qr.add_data(data)
-        qr.make(fit=True)
-        img = qr.make_image(fill_color="black", back_color="white")
-        svg_string = img.to_string().decode('utf-8').replace('width="37mm"', f'width="{size}px"').replace('height="37mm"', f'height="{size}px"')
-        return svg_string
-    
     async def generatePilImageAsync(self, data:str) -> PilImage:
         """
         Асинхронно генерирует QR-код на основе переданных данных.
@@ -78,5 +44,23 @@ class QrCodeService:
         img = qr.make_image(fill_color="black", back_color="white")
         svg_string = img.to_string().decode('utf-8').replace('width="37mm"', f'width="{size}px"').replace('height="37mm"', f'height="{size}px"')
         return svg_string
+
+    def generatePilImage(self, data:str) -> PilImage:
+        """
+        Генерирует QR-код на основе переданных данных.
+
+        :param data: Данные для кодирования в QR-код.
+        :return: Путь к сгенерированному QR-коду.
+        """
+        return asyncio.run(self.generatePilImageAsync(data))
+    
+    def generateSvg(self, data:str, size:int=200) -> str:
+        """
+        Генерирует QR-код в формате SVG на основе переданных данных (фиксированные размеры и стандартные атрибуты).
+
+        :param data: Данные для кодирования в QR-код.
+        :return: Строка с SVG-кодом QR-кода.
+        """
+        return asyncio.run(self.generateSvgAsync(data, size))
 
 qrcode_service = QrCodeService()
