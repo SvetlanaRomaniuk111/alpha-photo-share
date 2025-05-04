853d021cb15943d0a499dd5254ec9c375f260bbf
SvetlanaRomaniuk111 <gatskosv@gmail.com>
2025-05-04 20:01:57 +0300

    feat: Add CRUD operations for comments in the repository layer

--- src/api/comment/comments.py
+++ src/api/comment/comments.py
@@ -1,69 +1,79 @@
 from uuid import UUID
 from fastapi import APIRouter, HTTPException, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.future import select
-
 from src.db.database import get_db
-from src.models import User, Comment
+from src.models.posts import Comment
 from src.models.users import Role
 from src.schemas.comments import CommentSchema
+from src.models.users import User
 from src.services.auth import auth_service
 from src.services.roles import RoleAccessService
+from src.repository.posts import get_post
+from src.repository.comments import (
+    create_comment as create_comment_in_db,
+    get_comments as get_comments_from_db,
+    update_comment as update_comment_in_db,
+)
 
-router = APIRouter(prefix='/comments', tags=['comments'])
+router = APIRouter(prefix="/comments", tags=["comments"])
 all_roles_access = RoleAccessService([role for role in Role])
+admin_or_moderator_access = RoleAccessService([Role.admin, Role.moderator])
+
 
 @router.post("/", response_model=CommentSchema, status_code=status.HTTP_201_CREATED)
 async def create_comment(
-    post_id: UUID, message: str,
+    post_id: UUID,
+    message: str,
     user: User = Depends(auth_service.authenticate_user),
-    db: AsyncSession = Depends(get_db)
+    db: AsyncSession = Depends(get_db),
 ):
     if not message:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Message cannot be empty")
-    
-    comment = Comment(post_id=post_id, user_id=user.id, message=message)
-    db.add(comment)
-    await db.commit()
-    await db.refresh(comment)
-    return comment
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Message cannot be empty"
+        )
+    post = await get_post(post_id, db)
+    if not post:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Post not found"
+        )
+
+    return await create_comment_in_db(post_id, user.id, message, db)
 
 
 @router.get("/{post_id}", response_model=list[CommentSchema])
 async def get_comments(post_id: UUID, db: AsyncSession = Depends(get_db)):
-    stmt = select(Comment).filter(Comment.post_id == post_id).order_by(Comment.created_at)
-    result = await db.execute(stmt)
-    return result.scalars().all()
+    return await get_comments_from_db(post_id, db)
+
 
 @router.put("/{comment_id}", response_model=CommentSchema)
-async def edit_comment(
-    comment_id: UUID, message: str,
+async def update_comment(
+    comment_id: UUID,
+    message: str,
     user: User = Depends(auth_service.authenticate_user),
-    db: AsyncSession = Depends(get_db)
+    db: AsyncSession = Depends(get_db),
 ):
+    if not message:
+        raise HTTPException(status_code=400, detail="Message cannot be empty")
     comment = await db.get(Comment, comment_id)
     if not comment:
-        raise HTTPException(status_code=404, detail="Коментар не знайдено")
+        raise HTTPException(status_code=404, detail="Comment not found")
     if comment.user_id != user.id:
-        raise HTTPException(status_code=403, detail="Редагувати можна тільки свої коментарі")
+        raise HTTPException(
+            status_code=403, detail="You can only edit your own comments"
+        )
+    return await update_comment_in_db(comment_id, message, db)
 
-    comment.message = message
-    await db.commit()
-    await db.refresh(comment)
-    return comment
 
 @router.delete("/{comment_id}")
 async def delete_comment(
     comment_id: UUID,
-    user: User = Depends(auth_service.authenticate_user),
-    db: AsyncSession = Depends(get_db)
+    user: User = Depends(admin_or_moderator_access),
+    db: AsyncSession = Depends(get_db),
 ):
     comment = await db.get(Comment, comment_id)
     if not comment:
-        raise HTTPException(status_code=404, detail="Коментар не знайдено")
-    if user.role not in [Role.admin, Role.moderator]:
-        raise HTTPException(status_code=403, detail="Видаляти можуть тільки адміністратори")
+        raise HTTPException(status_code=404, detail="Comment not found")
 
     await db.delete(comment)
     await db.commit()
-    return {"message": "Коментар видалено"}
+    return {"message": "Comment deleted"}
